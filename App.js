c88bde9191ebdb59261711daac6060ab635a5350
AtharvaDPavesoft <atharvadhamkepavesoft@gmail.com>
2024-09-09 13:06:09 +0530

    Initialize project with basic app structure and components

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <Text>Hello World</Text>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
